caab8c63bf838d552cd31bdf9f9612c718c990e8
Travis CI <travis@sztukauniwersalna.pl>
2017-12-15 11:31:02 +0000

    Deploy to GitHub Pages: 5290b0ce3f80b4b2f8e200274e775220eb9dd8e8

--- entry-befb0655a50136835889.bundle.js
+++ entry-befb0655a50136835889.bundle.js
@@ -1483,6 +1483,7 @@ function getRootProps(locals, website, page) {
         page: page,
         localBundles: { css: css, js: js },
         externalBundles: { css: locals.css || [], js: locals.js || [] },
+        meta: locals.meta || [],
     };
 }
 
@@ -1493,10 +1494,18 @@ function getRootProps(locals, website, page) {
 
 "use strict";
 
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
 function Root(_a) {
-    var website = _a.website, page = _a.page, localBundles = _a.localBundles, externalBundles = _a.externalBundles;
+    var website = _a.website, page = _a.page, localBundles = _a.localBundles, externalBundles = _a.externalBundles, meta = _a.meta;
     return (React.createElement("html", null,
         React.createElement("head", null,
             React.createElement("title", null,
@@ -1513,7 +1522,8 @@ function Root(_a) {
             page.image !== null ? React.createElement("meta", { property: 'og:image', content: page.image }) : null,
             React.createElement("meta", { property: 'og:description', content: page.description }),
             React.createElement("meta", { property: 'og:locale', content: website.locale }),
-            React.createElement("meta", { property: 'og:type', content: page.url === '/' ? 'website' : 'article' })),
+            React.createElement("meta", { property: 'og:type', content: page.url === '/' ? 'website' : 'article' }),
+            meta.map(function (props, key) { return (React.createElement("meta", __assign({}, props, { key: key }))); })),
         React.createElement("body", null,
             React.createElement("div", { id: 'root' }, "%%%BODY%%%"),
             externalBundles.js.map(function (url) { return (React.createElement("script", { type: 'text/javascript', src: url, key: url })); }),
