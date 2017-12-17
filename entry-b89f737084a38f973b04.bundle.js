f45cef613bbe68baf79664947a72741280e17c73
Travis CI <travis@sztukauniwersalna.pl>
2017-12-17 19:30:46 +0000

    Deploy to GitHub Pages: 23d439d00f2a3e8163af87283122fa5019bcb464

--- entry-b89f737084a38f973b04.bundle.js
+++ entry-b89f737084a38f973b04.bundle.js
@@ -6233,23 +6233,70 @@ module.exports = {
 "use strict";
 
 
+var __extends = undefined && undefined.__extends || function () {
+    var extendStatics = Object.setPrototypeOf || { __proto__: [] } instanceof Array && function (d, b) {
+        d.__proto__ = b;
+    } || function (d, b) {
+        for (var p in b) {
+            if (b.hasOwnProperty(p)) d[p] = b[p];
+        }
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() {
+            this.constructor = d;
+        }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+}();
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(0);
 var s = __webpack_require__(107);
 var FB_PLUGINS_SERVICE = 'https://www.facebook.com/plugins/like.php';
-function FacebookLike(_a) {
-    var url = _a.url;
-    var params = {
-        href: url,
-        layout: 'button_count',
-        action: 'like',
-        size: 'large',
-        show_faces: false,
-        share: false,
-        height: 21
+var FacebookLike = /** @class */function (_super) {
+    __extends(FacebookLike, _super);
+    function FacebookLike(props) {
+        var _this = _super.call(this, props) || this;
+        _this.state = {
+            ready: false
+        };
+        _this.setReady = _this.setReady.bind(_this);
+        return _this;
+    }
+    FacebookLike.prototype.render = function () {
+        var _this = this;
+        var url = this.props.url;
+        var ready = this.state.ready;
+        var classNames = [s.like];
+        if (ready) {
+            classNames.push(s.ready);
+        }
+        var params = {
+            href: url,
+            layout: 'button_count',
+            action: 'like',
+            size: 'large',
+            show_faces: false,
+            share: false,
+            height: 21
+        };
+        return React.createElement("div", { className: classNames.join(' ') }, React.createElement("iframe", { src: FB_PLUGINS_SERVICE + "?" + urlParamsToString(params), scrolling: 'no', allowTransparency: true, ref: function ref(e) {
+                return _this.iframe = e;
+            } }));
     };
-    return React.createElement("div", { className: s.like }, React.createElement("iframe", { src: FB_PLUGINS_SERVICE + "?" + urlParamsToString(params), scrolling: 'no', allowTransparency: true }));
-}
+    FacebookLike.prototype.componentDidMount = function () {
+        this.iframe.addEventListener('load', this.setReady);
+    };
+    FacebookLike.prototype.componentWillUnmount = function () {
+        this.iframe.removeEventListener('load', this.setReady);
+    };
+    FacebookLike.prototype.setReady = function () {
+        this.setState(function (prev) {
+            return { ready: true };
+        });
+    };
+    return FacebookLike;
+}(React.Component);
 exports.FacebookLike = FacebookLike;
 exports.default = FacebookLike;
 function urlParamsToString(params) {
@@ -6263,7 +6310,8 @@ function urlParamsToString(params) {
 /***/ (function(module, exports) {
 
 module.exports = {
-  "like": "like-3LtG_"
+  "like": "like-3LtG_",
+  "ready": "ready-3Ze6P"
 };
 
 /***/ }),
