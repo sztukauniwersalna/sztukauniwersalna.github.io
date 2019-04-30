b35e9a7d977a8f94461e5c26efa518d9c3992c4b
Travis CI <travis@sztukauniwersalna.pl>
2019-04-30 12:06:58 +0000

    Deploy to GitHub Pages: ba654af6d8cf927f8502f34edabe005c8218d613

---
+++ 90-6ff9427a8c969fdfdccb.bundle.js
@@ -0,0 +1,101 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[90],{
+
+/***/ "./_posts/2018-12-06-fluxus-moodboard.markdown":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = __webpack_require__("react");
+var paramorph_1 = __webpack_require__("./node_modules/paramorph/index.js");
+/**
+ * @author Maciej Chałapuk (maciej@chalapuk.pl)
+ */
+var MarkdownPage = /** @class */ (function (_super) {
+    __extends(MarkdownPage, _super);
+    function MarkdownPage() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    MarkdownPage.prototype.render = function () {
+        var _a = this.context, paramorph = _a.paramorph, page = _a.page, history = _a.history;
+        return (React.createElement(paramorph_1.Content, __assign({ limit: 5 }, this.props),
+            React.createElement("p", null,
+                "Fluxus (\u0142ac. p\u0142yn\u0105cy; ang. to flow) nie by\u0142 skonkretyzowanym kierunkiem w sztuce, a raczej ruchem, artystycznym manifestem artyst\u00F3w \u017Cyj\u0105cych w latach 60-tych, 70-tych XX wieku. Charakterystyczny humor, nawi\u0105zanie do ruchu ",
+                React.createElement("a", { href: "http://sztukauniwersalna.pl/2018-04-05-dadaizm-moodboard" }, "DADA"),
+                " pozwoli\u0142o na przekraczanie granic i wzajemnie przenikanie si\u0119 dziedzin sztuki. Za flux-ojca uwa\u017Cany jest George Maciunas, ameryka\u0144ski artysta litewskiego pochodzenia. Pierwszy z mi\u0119dzynarodowych festiwali, Fluxus International Festspiele, odby\u0142 si\u0119 w Muzeum Wiesbaden w 1962 roku. Na festiwal sk\u0142ada\u0142y si\u0119 g\u0142\u00F3wnie koncerty po\u0142\u0105czone ze sztuk\u0105 akcji, tzw. happeningami. Fluxus znacz\u0105co przyczyni\u0142 si\u0119 do popularyzacji i rozwoju sztuki performance, muzyki eksperymentalnej, konceptualizmu, mail art\u2019u (sztuki poczty). Podczas flux-koncert\u00F3w \u0142amano wszelkie zasady, a na ich miejscu pojawia\u0142y si\u0119 nieprzewidziane eksperymenty z form\u0105 i tre\u015Bci\u0105. Pi\u0142owano fortepian, niszczono skrzypce, policzkowano kompozytora zgodnie z zaleceniami partytury. Idee Fluxus do swojej tw\u00F3rczo\u015Bci wciela\u0142/a John Cage, George Brecht, Dick Higgins, Nam June Paik, Yoko Ono."),
+            React.createElement("p", null, "George Maciunas w manife\u015Bcie Fluxus pisa\u0142:"),
+            React.createElement("blockquote", null,
+                React.createElement("p", null, "Nale\u017Cy promowa\u0107 rewolucyjn\u0105 pow\u00F3d\u017A (...) sztuk\u0119 \u017Cyw\u0105, anty-sztuk\u0119, nieartystyczn\u0105 rzeczywisto\u015B\u0107, kt\u00F3r\u0105 chwyc\u0105 wszyscy ludzi, nie tylko krytycy, dyletanci i profesjonali\u015Bci\u201D. (kopie manifestu zosta\u0142y rzucone w publiczno\u015B\u0107 podczas Festum Fluxorum Fluxus maj\u0105cym miejsce w 1963 roku w Dusseldorfie)")),
+            React.createElement("p", null, "Dzia\u0142ania Fluxus\u2019u cechuje spontaniczno\u015B\u0107, dopuszczenie przypadku jako cz\u0119\u015Bci aktu tw\u00F3rczego, amatorsko\u015B\u0107, zabawa, refleksja."),
+            React.createElement("p", null, "Za koniec Fluxus\u2019u uwa\u017Ca si\u0119 moment, kiedy zmar\u0142 jego lider George Maciunas. Ceremonia pogrzebowa odby\u0142a si\u0119 w charakterystycznym flux-stylu. Jedzenie mia\u0142o tylko i wy\u0142\u0105cznie kolor czarny, bia\u0142y lub fioletowy. Jednak czy Fluxus na pewno si\u0119 zako\u0144czy\u0142? Jego spu\u015Bcizna nieustannie dzia\u0142a na m\u0142odych tw\u00F3rc\u00F3w inspiruj\u0105c do eksperymentalnych dzia\u0142a\u0144. To w\u0142a\u015Bnie dzi\u0119ki Fluxus'owi mo\u017Cemy dzi\u015B studiowa\u0107 kierunek 'intermedia' na wielu wy\u017Cszych uczelniach w kraju i poza jego granicami."),
+            React.createElement("p", null,
+                React.createElement("img", { src: "https://assets2.ello.co/uploads/asset/attachment/8596053/ello-optimized-b16c9890.jpg", alt: "Obraz przedstawia trzy czarno-bia\u0142e grafiki na szarym tle." }),
+                React.createElement("img", { src: "https://assets2.ello.co/uploads/asset/attachment/8596056/ello-optimized-fddc1289.jpg", alt: "Obraz przedstawia cztery fotografie przedmiot\u00F3w na szarym tle." }),
+                React.createElement("img", { src: "https://assets1.ello.co/uploads/asset/attachment/8596060/ello-optimized-a429436d.jpg", alt: "Obraz przedstawia cztery fotografie. Widzimy czarno-bia\u0142\u0105 fotografi\u0119 kobiety i m\u0119\u017Cczyzny, oraz fotografie dzie\u0142 znanych artyst\u00F3w." }),
+                React.createElement("img", { src: "https://assets0.ello.co/uploads/asset/attachment/8596064/ello-optimized-33f18cdf.jpg", alt: "Obraz przedstawia dwie czarno-bia\u0142e fotografie na szarym tle. Na zdj\u0119ciach widzimy kobiec\u0105 posta\u0107." }),
+                React.createElement("img", { src: "https://assets2.ello.co/uploads/asset/attachment/8596074/ello-optimized-4911291f.jpg", alt: "Obraz przedstawia trzy czarno-bia\u0142e fotografie, oraz jedno zdj\u0119cie tekstu na bia\u0142ym papierze. Ca\u0142o\u015B\u0107 na szarym tle." }),
+                React.createElement("img", { src: "https://assets0.ello.co/uploads/asset/attachment/8596080/ello-optimized-3c365d1c.jpg", alt: "Obraz przedstawia trzy fotografie, z czego jedna jest fotografi\u0105 kolorow\u0105. Ca\u0142o\u015B\u0107 na szarym tle." }),
+                React.createElement("img", { src: "https://assets0.ello.co/uploads/asset/attachment/8596082/ello-optimized-40b8a51c.jpg", alt: "Obraz przedstawia cztery fotografie w szarych odcieniach. Ca\u0142o\u015B\u0107 na szarym tle." }),
+                React.createElement("img", { src: "https://assets1.ello.co/uploads/asset/attachment/8596224/ello-optimized-c680e0d6.jpg", alt: "Obraz przedstawia jasny tekst na szarym tle." }))));
+    };
+    return MarkdownPage;
+}(paramorph_1.PureComponent));
+exports.MarkdownPage = MarkdownPage;
+exports.default = MarkdownPage;
+
+
+/***/ }),
+
+/***/ "./node_modules/paramorph/index.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var model_1 = __webpack_require__("./node_modules/paramorph/model/index.js");
+exports.Layout = model_1.Layout;
+exports.Include = model_1.Include;
+exports.Page = model_1.Page;
+exports.Category = model_1.Category;
+exports.Collection = model_1.Collection;
+exports.Tag = model_1.Tag;
+exports.Paramorph = model_1.Paramorph;
+var react_1 = __webpack_require__("./node_modules/paramorph/react/index.js");
+exports.Content = react_1.Content;
+exports.ContextTypes = react_1.ContextTypes;
+exports.ContextContainer = react_1.ContextContainer;
+exports.Component = react_1.Component;
+exports.PureComponent = react_1.PureComponent;
+exports.Link = react_1.Link;
+exports.DeferredStyles = react_1.DeferredStyles;
+exports.DeferredScripts = react_1.DeferredScripts;
+exports.Root = react_1.Root;
+
+
+/***/ })
+
+}]);
---
+++ gtagConfig-5141e00f0febe0282ea3.bundle.js
@@ -0,0 +1,143 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./node_modules/parrot-layout/gtagConfig.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function(global) {!function(e,t){for(var n in t)e[n]=t[n]}(exports,function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=64)}({11:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=window.dataLayer=window.dataLayer||[];function o(e){for(var t=[],n=1;n<arguments.length;n++)t[n-1]=arguments[n];r.push(arguments)}t.gtag=o,t.default=o},64:function(e,t,n){e.exports=n(65)},65:function(e,t,n){"use strict";var r=n(11); true&&(r.default("js",new Date),r.default("config","UA-110945340-1"),window.addEventListener("load",function(){var e=location.pathname;global.setInterval(function(){e!==location.pathname&&(e=location.pathname,r.default("config","UA-110945340-1",{page_path:e}),console.log("page view: "+e))},300)})),e.exports={}}}));
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__("./node_modules/webpack/buildin/global.js")))
+
+/***/ }),
+
+/***/ "./node_modules/webpack/buildin/global.js":
+/***/ (function(module, exports) {
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || new Function("return this")();
+} catch (e) {
+	// This works if the window reference is available
+	if (typeof window === "object") g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+
+
+/***/ }),
+
+/***/ 2:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./node_modules/parrot-layout/gtagConfig.js");
+
+
+/***/ })
+
+/******/ });
+});
