378b85db9491d53348dc8f9cf04392789c1429b4
Travis CI <travis@sztukauniwersalna.pl>
2019-07-15 03:06:39 +0000

    Deploy to GitHub Pages: ff215fd305241b3467b78b4487f9f7705c85509d

--- 0-400fa61d52908cc1174a.bundle.js
+++ 0-400fa61d52908cc1174a.bundle.js
@@ -1235,6 +1235,7 @@ function Root(_a) {
     var deferredStyles = externalBundles.css;
     return (React.createElement("html", { className: 'noscript' },
         React.createElement("head", null,
+            React.createElement("meta", { httpEquiv: 'Content-Security-Policy', content: "default-src *;" }),
             React.createElement("title", null,
                 post.title,
                 " | ",
@@ -1242,7 +1243,6 @@ function Root(_a) {
             React.createElement("meta", { name: 'keywords', content: post.tags.join(', ') }),
             React.createElement("meta", { name: 'description', content: post.description }),
             React.createElement("meta", { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }),
-            React.createElement("meta", { httpEquiv: 'Content-Security-Policy', content: "default-src 'self' ; script-src 'self' 'unsafe-inline' *.disqus.com a.disquscdn.com requirejs.org www.google-analytics.com; style-src 'self' 'unsafe-inline' a.disquscdn.com; img-src 'self' *; media-src 'self' ; frame-src disqus.com;" }),
             React.createElement("script", { async: true, type: "text/javascript", src: GA_API_URL }),
             React.createElement("script", { async: true, type: 'text/javascript', src: GTAG_API_URL }),
             React.createElement("script", { type: 'text/javascript', src: gtagConfigScript }),
